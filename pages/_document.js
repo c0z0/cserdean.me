1670dd84febf9ede57198934f1ec0836176c3e30
Cosmin Serdean <cosmoserdean@gmail.com>
2018-01-21 23:33:40 +0200

    Propper favicons

--- pages/_document.js
+++ pages/_document.js
@@ -16,11 +16,29 @@ export default class MyDocument extends Document {
 						rel="stylesheet"
 					/>
 					<link
+						rel="apple-touch-icon"
+						sizes="180x180"
+						href="/static/apple-touch-icon.png"
+					/>
+					<link
 						rel="icon"
 						type="image/png"
 						sizes="32x32"
-						href={`/static/favicon-${favicon}.png`}
+						href="/static/favicon-32x32.png"
 					/>
+					<link
+						rel="icon"
+						type="image/png"
+						sizes="16x16"
+						href="/static/favicon-16x16.png"
+					/>
+					<link rel="manifest" href="/static/manifest.json" />
+					<link rel="shortcut icon" href="/static/favicon.ico" />
+					<meta
+						name="msapplication-config"
+						content="/static/browserconfig.xml"
+					/>
+					<meta name="theme-color" content="#ffffff" />
 					<title>Cosmin Serdean</title>
 					<style>{`body { margin: 0; font-family: Lato; }`}</style>
 				</Head>
