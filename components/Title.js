1ee7c7bdba34f6dd7bf42e498142c11b5b99ac13
Cosmin Serdean <cosmoserdean@gmail.com>
2018-01-22 21:37:46 +0200

    Removed visibility sensor

--- components/Title.js
+++ components/Title.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react'
-import VisibilitySensor from 'react-visibility-sensor'
+import Link from 'next/link'
 
 import { darkBlue as accentColor, text as textColor } from '../utils/colors'
 
 export default class Title extends Component {
 	state = {
-		visible: true,
 		title:
 			"Hi, I'm Cosmin Serdean and I'm a software developer. I like to build stuff with React and Node. I also enjoy doing magic with Neural Networks.",
 		remove: 94,
@@ -15,8 +14,7 @@ export default class Title extends Component {
 
 	onPress({ keyCode, key }) {
 		let { remove, typed } = this.state
-		const { title, visible } = this.state
-		if (!visible) return
+		const { title } = this.state
 		if (keyCode == 8) {
 			typed = true
 			remove = remove >= title.length ? remove : remove + 1
@@ -75,193 +73,190 @@ export default class Title extends Component {
 		const titleP4 = shortenedTitle.slice(magicEnd)
 
 		return (
-			<VisibilitySensor
-				partialVisibility
-				onChange={visible => this.setState({ visible })}
-			>
-				<div className="container">
-					<a
-						href="https://github.com/c0z0/cserdean.me"
-						className="src"
-						target="_blank"
-					>
-						[src]
-					</a>
-					{!isMobile && (
-						<p className="title">
-							{titleP1}
-							<span className="title__name">{titleBold}</span>
-							{titleP2}
-							<a href="http://github.com/c0z0/lollio" className="title__link">
+			<div className="container">
+				<a
+					href="https://github.com/c0z0/cserdean.me"
+					className="src"
+					target="_blank"
+				>
+					[src]
+				</a>
+				{!isMobile && (
+					<p className="title">
+						{titleP1}
+						<span className="title__name">{titleBold}</span>
+						{titleP2}
+						<Link href="/projects" prefetch>
+							<a href="" className="title__link">
 								{stuffText}
 							</a>
-							{titleP3}
-							<a
-								href="http://github.com/c0z0/contrast_neural_net"
-								className="title__link"
-							>
-								{magicText}
-							</a>
-							{titleP4}
-							<span className="title__block">|</span>
-						</p>
-					)}
-					{!isMobile && (
-						<p className={`tooltip ${showTooltip ? 'tooltip--visible' : ''}`}>
-							try typing
-						</p>
-					)}
-					{isMobile && (
-						<p className="title title--mobile">
-							{' '}
-							{titleP1}
-							<span className="title__name">{titleBold}</span>
-							{titleP2}
-							<a href="http://github.com/c0z0/lollio" className="title__link">
-								{stuffText}
-							</a>
-							{titleP3}
-							<a
-								href="http://github.com/c0z0/contrast_neural_net"
-								className="title__link"
-							>
-								{magicText}
-							</a>
-							{titleP4}
-						</p>
-					)}
-					<div className="menu">
-						<a href="mailto:cosmoserdean@gmail.com" className="menu__item">
-							Email
-						</a>
-						<a href="/static/cv_cosmin_serdean.pdf" className="menu__item">
-							CV
-						</a>
+						</Link>
+						{titleP3}
 						<a
-							href="https://github.com/c0z0"
-							target="_blank"
-							className="menu__item"
+							href="http://github.com/c0z0/contrast_neural_net"
+							className="title__link"
 						>
-							GitHub
+							{magicText}
 						</a>
-					</div>
-					<style jsx>{`
-						.menu {
-							position: absolute;
-							bottom: 16px;
-						}
+						{titleP4}
+						<span className="title__block">|</span>
+					</p>
+				)}
+				{!isMobile && (
+					<p className={`tooltip ${showTooltip ? 'tooltip--visible' : ''}`}>
+						try typing
+					</p>
+				)}
+				{isMobile && (
+					<p className="title title--mobile">
+						{' '}
+						{titleP1}
+						<span className="title__name">{titleBold}</span>
+						{titleP2}
+						<a href="http://github.com/c0z0/lollio" className="title__link">
+							{stuffText}
+						</a>
+						{titleP3}
+						<a
+							href="http://github.com/c0z0/contrast_neural_net"
+							className="title__link"
+						>
+							{magicText}
+						</a>
+						{titleP4}
+					</p>
+				)}
+				<div className="menu">
+					<a href="mailto:cosmoserdean@gmail.com" className="menu__item">
+						Email
+					</a>
+					<a href="/static/cv_cosmin_serdean.pdf" className="menu__item">
+						CV
+					</a>
+					<a
+						href="https://github.com/c0z0"
+						target="_blank"
+						className="menu__item"
+					>
+						GitHub
+					</a>
+				</div>
+				<style jsx>{`
+					.menu {
+						position: absolute;
+						bottom: 16px;
+					}
 
-						.menu :hover {
+					.menu :hover {
+						opacity: 1;
+					}
+
+					.menu__item {
+						margin: 0 16px;
+						font-size: 0.8em;
+						text-decoration: none;
+						opacity: 0.5;
+						color: ${accentColor} !important;
+						transition: all 0.2s;
+						font-family: Menlo;
+					}
+
+					.tooltip {
+						margin: 0;
+						font-size: 0.8em;
+						opacity: 0;
+					}
+
+					.tooltip--visible {
+						opacity: 0.4;
+						animation: fadein 0.5s;
+					}
+
+					.src {
+						font-size: 0.8em;
+						text-decoration: none;
+						position: absolute;
+						top: 16px;
+						left: 16px;
+						opacity: 0.5;
+						color: ${accentColor} !important;
+						transition: all 0.2s;
+						font-family: Menlo;
+					}
+
+					.src:hover {
+						opacity: 1;
+					}
+
+					.title__block {
+						font: Menlo;
+						color: ${accentColor};
+						font-size: 1.1em;
+						animation: cursor 1s infinite;
+					}
+
+					.title__name {
+						font-family: Menlo;
+						text-decoration: underline;
+					}
+
+					.title {
+						margin: 16px 128px;
+						text-align: justify;
+					}
+
+					.title--mobile {
+						margin: 0 16px;
+					}
+
+					.container {
+						position: relative;
+						height: 100vh;
+						color: ${textColor};
+						display: flex;
+						align-items: center;
+						justify-content: center;
+						flex-direction: column;
+					}
+
+					.title__link {
+						transition: all 0.2s;
+						color: ${textColor} !important;
+					}
+
+					.title__link:hover {
+						color: ${accentColor} !important;
+					}
+
+					@keyframes cursor {
+						0% {
+							opacity: 0;
+						}
+						40% {
+							opacity: 0;
+						}
+						50% {
 							opacity: 1;
 						}
-
-						.menu__item {
-							margin: 0 16px;
-							font-size: 0.8em;
-							text-decoration: none;
-							opacity: 0.5;
-							color: ${accentColor} !important;
-							transition: all 0.2s;
-							font-family: Menlo;
+						90% {
+							opacity: 1;
 						}
+						100% {
+							opacity: 0;
+						}
+					}
 
-						.tooltip {
-							margin: 0;
-							font-size: 0.8em;
+					@keyframes fadein {
+						0% {
 							opacity: 0;
 						}
 
-						.tooltip--visible {
+						100% {
 							opacity: 0.4;
-							animation: fadein 0.5s;
 						}
-
-						.src {
-							font-size: 0.8em;
-							text-decoration: none;
-							position: absolute;
-							top: 16px;
-							left: 16px;
-							opacity: 0.5;
-							color: ${accentColor} !important;
-							transition: all 0.2s;
-							font-family: Menlo;
-						}
-
-						.src:hover {
-							opacity: 1;
-						}
-
-						.title__block {
-							font: Menlo;
-							color: ${accentColor};
-							font-size: 1.1em;
-							animation: cursor 1s infinite;
-						}
-
-						.title__name {
-							font-family: Menlo;
-							text-decoration: underline;
-						}
-
-						.title {
-							margin: 16px 128px;
-							text-align: justify;
-						}
-
-						.title--mobile {
-							margin: 0 16px;
-						}
-
-						.container {
-							position: relative;
-							height: 100vh;
-							color: ${textColor};
-							display: flex;
-							align-items: center;
-							justify-content: center;
-							flex-direction: column;
-						}
-
-						.title__link {
-							transition: all 0.2s;
-							color: ${textColor} !important;
-						}
-
-						.title__link:hover {
-							color: ${accentColor} !important;
-						}
-
-						@keyframes cursor {
-							0% {
-								opacity: 0;
-							}
-							40% {
-								opacity: 0;
-							}
-							50% {
-								opacity: 1;
-							}
-							90% {
-								opacity: 1;
-							}
-							100% {
-								opacity: 0;
-							}
-						}
-
-						@keyframes fadein {
-							0% {
-								opacity: 0;
-							}
-
-							100% {
-								opacity: 0.4;
-							}
-						}
-					`}</style>
-				</div>
-			</VisibilitySensor>
+					}
+				`}</style>
+			</div>
 		)
 	}
 }
