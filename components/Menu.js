07a1fc5a7b0c8a44746eb0fdccfb5bd242c0c92e
Cosmin Serdean <cosmoserdean@gmail.com>
2019-01-20 23:36:23 +0200

    Menu change

--- components/Menu.js
+++ components/Menu.js
@@ -1,57 +1,56 @@
-import React, { useState } from "react";
-import Link from "next/link";
-import PropTypes from "prop-types";
+import React, {useState} from 'react'
+import Link from 'next/link'
+import PropTypes from 'prop-types'
 
-import { pink, text } from "../utils/colors";
-import * as breakPoints from "../utils/breakPoints";
+import {pink, blue} from '../utils/colors'
+import * as breakPoints from '../utils/breakPoints'
 
 const items = [
   {
-    title: "Home",
-    target: "/"
+    title: 'Home',
+    target: '/',
   },
   {
-    title: "Projects",
-    target: "/projects"
+    title: 'Projects',
+    target: '/projects',
   },
   {
-    title: "AI Experiments",
-    target: "/ai"
+    title: 'AI Experiments',
+    target: '/ai',
   },
   {
-    title: "CV",
-    target: "/static/cv_cosmin_serdean.pdf",
-    right: true
-  },
-  {
-    title: "GitHub",
-    target: "https://github.com/c0z0",
-    right: true
-  },
-  {
-    title: "Email",
-    target: "mailto:cosmoserdean@gmail.com",
+    title: 'CV',
+    target: '/static/cv_cosmin_serdean.pdf',
     right: true,
-    primary: true
-  }
-];
+  },
+  {
+    title: 'GitHub',
+    target: 'https://github.com/c0z0',
+    right: true,
+  },
+  {
+    title: 'Email',
+    target: 'mailto:cosmoserdean@gmail.com',
+    right: true,
+    primary: true,
+  },
+]
 
-export default function Menu({ active, dark }) {
-  const [open, setMenu] = useState(false);
+export default function Menu({active, dark}) {
+  const [open, setMenu] = useState(false)
 
-  const textColor = !dark ? "#484848" : "white";
+  const textColor = !dark ? '#484848' : 'white'
 
-  const leftItems = items.filter(({ right }) => !right);
+  const leftItems = items.filter(({right}) => !right)
 
-  const rightItems = items.filter(({ right }) => right);
+  const rightItems = items.filter(({right}) => right)
   return (
     <header>
       <div className="menu__wrapper">
         <div className="menu__logo__wrapper">
           <a
             href="https://github.com/c0z0/cserdean.me"
-            rel="noopener noreferrer"
-          >
+            rel="noopener noreferrer">
             <img
               src="/static/triangle-gs.svg"
               alt="logo"
@@ -61,40 +60,37 @@ export default function Menu({ active, dark }) {
           <button
             onClick={() => setMenu(!open)}
             type="button"
-            className="menu__button"
-          >
+            className="menu__button">
             <div className="menu__icon">
               <div className="menu__icon__top" />
               <div className="menu__icon__bottom" />
             </div>
           </button>
         </div>
-        <div className={`menu ${open ? "menu--open" : ""}`}>
+        <div className={`menu ${open ? 'menu--open' : ''}`}>
           <div className="menu__left">
-            {leftItems.map(({ title, target, primary, right }) => (
+            {leftItems.map(({title, target, primary, right}) => (
               <Link href={target} prefetch={!right} key={target}>
                 <a
                   href={target}
                   className={`menu__item ${
-                    active === target ? "menu__item--active" : ""
-                  } ${primary ? "menu__item--primary" : ""}`}
-                >
+                    active === target ? 'menu__item--active' : ''
+                  } ${primary ? 'menu__item--primary' : ''}`}>
                   {title}
                 </a>
               </Link>
             ))}
           </div>
           <div className="menu__right">
-            {rightItems.map(({ title, target, primary }) => (
+            {rightItems.map(({title, target, primary}) => (
               <a
                 key={target}
                 href={target}
                 className={`menu__item ${
-                  active === target ? "menu__item--active" : ""
-                } ${primary ? "menu__item--primary" : ""}`}
+                  active === target ? 'menu__item--active' : ''
+                } ${primary ? 'menu__item--primary' : ''}`}
                 target="_blank"
-                rel="noopener noreferrer"
-              >
+                rel="noopener noreferrer">
                 {title}
               </a>
             ))}
@@ -102,8 +98,8 @@ export default function Menu({ active, dark }) {
         </div>
         <style jsx>{`
           .menu__wrapper {
-            font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto",
-              "Oxygen", "Ubuntu", "Cantarell", "Fira Sans";
+            font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto',
+              'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans';
             margin-top: 20px;
           }
 
@@ -114,12 +110,12 @@ export default function Menu({ active, dark }) {
             display: block;
             padding: 16px;
             padding-left: 20px;
-            border-bottom: 1px solid ${!dark ? "#eee" : "#222"};
+            border-bottom: 1px solid ${!dark ? '#eee' : '#222'};
             text-transform: uppercase;
           }
 
           .menu__item--active {
-            background: ${!dark ? "#eee" : "#222"};
+            background: ${!dark ? '#eee' : '#222'};
           }
 
           .menu__item--primary {
@@ -171,13 +167,13 @@ export default function Menu({ active, dark }) {
           }
 
           .menu__icon__top {
-            transform: rotate(${open ? "45deg" : 0})
-              translateY(${open ? "1px" : "-4px"});
+            transform: rotate(${open ? '45deg' : 0})
+              translateY(${open ? '1px' : '-4px'});
           }
 
           .menu__icon__bottom {
-            transform: rotate(${open ? "-45deg" : 0})
-              translateY(${open ? "-1px" : "4px"});
+            transform: rotate(${open ? '-45deg' : 0})
+              translateY(${open ? '-1px' : '4px'});
           }
 
           @media (${breakPoints.tabletUp}) {
@@ -202,8 +198,8 @@ export default function Menu({ active, dark }) {
               justify-content: space-between;
               font-size: 12px;
               flex: 1;
-              font-family: -apple-system, BlinkMacSystemFont, "Segoe UI",
-                "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans";
+              font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI',
+                'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans';
             }
 
             .menu__item {
@@ -217,7 +213,7 @@ export default function Menu({ active, dark }) {
             }
 
             .menu__item--active {
-              color: ${!dark ? "black" : pink};
+              color: ${!dark ? blue : pink};
               background: none;
             }
 
@@ -232,14 +228,14 @@ export default function Menu({ active, dark }) {
         `}</style>
       </div>
     </header>
-  );
+  )
 }
 
 Menu.propTypes = {
   active: PropTypes.oneOf(items.map(i => i.target)).isRequired,
-  dark: PropTypes.bool
-};
+  dark: PropTypes.bool,
+}
 
 Menu.defaultProps = {
-  dark: false
-};
+  dark: false,
+}
