37632864fb4e5bdb3004a174d802d6fe36fc518e
Cosmin Serdean <cosmoserdean@gmail.com>
2019-01-20 23:36:50 +0200

    Projects page

--- pages/ai/index.js
+++ pages/ai/index.js
@@ -1,18 +1,26 @@
-import React from "react";
+import React from 'react'
+import Link from 'next/link'
 
-import Page from "../../components/Page";
-import * as colors from "../../utils/colors";
+import Page from '../../components/Page'
+import * as colors from '../../utils/colors'
 
 const Ai = () => (
   <Page active="/ai" dark>
     <h3 className="title">AI Experiments</h3>
     <p className="subtitle">Tiny Machine Learning demos</p>
     <div className="experiments">
-      <div className="experiment">
-        <a className="experiment__anchor" href="/ai/sentiment">
-          Sentiment classifier trained on movie reviews
+      <p className="experiment">
+        <Link href="/ai/sentiment" prefetch>
+          <a className="experiment__anchor" href="/ai/sentiment">
+            Sentiment classifier trained on movie reviews
+          </a>
+        </Link>
+        <a
+          className="experiment__src"
+          href="https://github.com/c0z0/sentiment-classifier">
+          [src]
         </a>
-      </div>
+      </p>
       {/* <div className="experiment">
         <a className="experiment__anchor" href="/ai/sentiment">
           Neural English to German translation
@@ -20,6 +28,13 @@ const Ai = () => (
       </div> */}
     </div>
     <style jsx>{`
+      .experiment__src {
+        color: ${colors.blue};
+        float: right;
+        text-decoration: none;
+        font-family: Menlo;
+      }
+
       .title {
         font-size: 2.5em;
         margin-bottom: 12px;
@@ -48,6 +63,6 @@ const Ai = () => (
       }
     `}</style>
   </Page>
-);
+)
 
-export default Ai;
+export default Ai
