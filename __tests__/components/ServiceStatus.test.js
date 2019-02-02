d359e4c46bdd739b3a9008b220ef1a2deb3f2dd2
Cosmin Serdean <cosmoserdean@gmail.com>
2019-02-02 19:02:58 +0200

    Add service status test

--- __tests__/components/ServiceStatus.test.js
+++ __tests__/components/ServiceStatus.test.js
@@ -1,31 +1,45 @@
-import React from "react";
-import { render, cleanup, waitForDomChange } from "react-testing-library";
-import "jest-styled-components";
+import React from 'react'
+import {render, cleanup, waitForDomChange} from 'react-testing-library'
+import 'jest-styled-components'
 
-import ServiceStatus from "../../components/ServiceStatus";
+import ServiceStatus from '../../components/ServiceStatus'
 
-afterEach(cleanup);
+afterEach(cleanup)
 
-test("Changes color to green when ready", async () => {
-  global.fetch = jest.fn(async () => ({ ok: true }));
+test('Changes color to green when ready', async () => {
+  global.fetch = jest.fn(async () => ({ok: true}))
 
-  const { container } = render(<ServiceStatus />);
+  const {container} = render(<ServiceStatus />)
 
-  expect(container.firstChild).toHaveStyleRule("background", "#f4d142");
+  expect(container.firstChild).toHaveStyleRule('background', '#f4d142')
 
-  await waitForDomChange({ container });
+  await waitForDomChange({container})
 
-  expect(container.firstChild).toHaveStyleRule("background", "#5ff441");
-});
+  expect(container.firstChild).toHaveStyleRule('background', '#5ff441')
+})
 
-test("Changes color to green when errors out", async () => {
-  global.fetch = jest.fn(async () => ({ ok: false }));
+test("Changes color to red when response isn't ok", async () => {
+  global.fetch = jest.fn(async () => ({ok: false}))
 
-  const { container } = render(<ServiceStatus />);
+  const {container} = render(<ServiceStatus />)
 
-  expect(container.firstChild).toHaveStyleRule("background", "#f4d142");
+  expect(container.firstChild).toHaveStyleRule('background', '#f4d142')
 
-  await waitForDomChange({ container });
+  await waitForDomChange({container})
 
-  expect(container.firstChild).toHaveStyleRule("background", "#ff001f");
-});
+  expect(container.firstChild).toHaveStyleRule('background', '#ff001f')
+})
+
+test('Changes color to red when throws error', async () => {
+  global.fetch = jest.fn(async () => {
+    throw new Error('Mocked error')
+  })
+
+  const {container} = render(<ServiceStatus />)
+
+  expect(container.firstChild).toHaveStyleRule('background', '#f4d142')
+
+  await waitForDomChange({container})
+
+  expect(container.firstChild).toHaveStyleRule('background', '#ff001f')
+})
